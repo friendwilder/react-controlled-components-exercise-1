d23e54c4d6aa0495d93d9982cbdac3acc0d0ccd9
Wilder <friendwilder@yahoo.com.mx>
2021-01-13 00:41:19 +0000

    Adding state but not updating

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state = {
+    query: ''
+  }
+  
+  updateQuery = (query) => {
+  	this.setState(() => ({
+      query: query.trim()
+    }))
+  }
+  
   render() {
     return (
       <div className="App">
@@ -11,7 +22,12 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input 
+			type="text" 
+			placeholder="Say Something"
+			value={this.state.query}
+			onChange={(event) => this.updateQuery(event.target.value)}
+			/>
           <p className="echo">Echo:</p>
           <p>This should mirror the text you typed into the input field.</p>
         </div>
